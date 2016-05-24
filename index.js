dee070b104f354d0aa1a528cde603647e5a6ad0f
jasonhkennedy <jharlkennedy@gmail.com>
2016-05-24 10:44:25 -0500

    Better console message on start.

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Bowling Scorer listening on port ", port)
 
 })
